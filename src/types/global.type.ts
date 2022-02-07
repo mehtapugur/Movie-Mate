cdb09a4e81791eaf68366a7c956db578f7269cec
Mehtap Ugur <yaziliminval@hotmail.com>
2022-02-08 01:49:37 +0300

    Update global.type.ts

--- src/types/global.type.ts
+++ src/types/global.type.ts
@@ -3,3 +3,7 @@ import { NextFunction, Request, Response } from "express";
 type middleware = (req: Request, res: Response, next: NextFunction) => void;
 
 const login: middleware = (req, res, next) => {};
+
+declare global {
+  var userIN: null;
+}
